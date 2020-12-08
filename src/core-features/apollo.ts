8d56612fa7ed7ec43202b8bbd7e7337baa2e2979
Ahsan-J <ahsan.ahmed99@ymail.com>
2020-12-09 00:24:36 +0500

    removed key

--- src/core-features/apollo.ts
+++ src/core-features/apollo.ts
@@ -3,7 +3,7 @@ import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
 const httpLink = createHttpLink({
   uri: 'https://api.github.com/graphql',
   headers: {
-    authorization: `bearer ${process.env.GIT_TOKEN || "c2999e1b200229a3690d90855b702fd1ee8c1f02"}`
+    authorization: `bearer ${process.env.GIT_TOKEN}`
   },
 });
 
