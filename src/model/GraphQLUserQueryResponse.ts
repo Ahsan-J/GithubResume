ca77e734ac73f550c63d6f4a8f406840f10bdcea
Ahsan Ahmed <ahsan.ahmed99@ymail.com>
2024-01-19 18:27:50 +0500

    typing added for GitResume

---
+++ src/model/GraphQLUserQueryResponse.ts
@@ -0,0 +1,40 @@
+export interface IGitUser {
+    login: string;
+    id: string;
+    name: string;
+    bio: string;
+    websiteUrl: string;
+    createdAt: string;
+    location: string;
+    repositories: IGitItemList<IGitRepository>;
+    followers: IGitItemList;
+    pinnedItems: IGitItemList<IGitRepository>;
+}
+
+interface IGitItemList<T = undefined> {
+    totalCount: number;
+    nodes: T extends undefined ? undefined : Array<T>;
+}
+
+export interface IGitRepository {
+    "name": string,
+    "viewerPermission": "ADMIN",
+    "description": string,
+    "forkCount": number,
+    "stargazers": {
+        "totalCount": number,
+        "__typename": "StargazerConnection"
+    },
+    "url": string,
+    "createdAt": string,
+    "pushedAt": string,
+    "primaryLanguage": IGitRepositoryLanguage,
+    "__typename": "Repository"
+}
+
+export interface IGitRepositoryLanguage {
+    "color": string,
+    "id": string,
+    "name": string,
+    "__typename": "Language"
+}
